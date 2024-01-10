6a37161588b0c9612a0383f7c0e1ec1fdd3f5561
BDanielo <daniel@armieux.fr>
2024-01-10 17:01:19 +0100

    feat(Tag) : repair post and put endpoints

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -3,13 +3,71 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\TagRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TagRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(
+            normalizationContext: ['groups' => ['tag:read']]
+        ),
+        new Post(
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                'properties' => [
+                                    'name' => [
+                                        'type' => 'string',
+                                        'example' => 'Sport'
+                                    ],
+                                    'description' => [
+                                        'type' => 'string',
+                                        'example' => 'Toutes les activités sportives'
+                                    ]
+                                ],
+                            ]
+                        ]
+                    ])
+                )
+            )
+        ),
+        new Put(
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                'properties' => [
+                                    'name' => [
+                                        'type' => 'string',
+                                        'example' => 'Sport'
+                                    ],
+                                    'description' => [
+                                        'type' => 'string',
+                                        'example' => 'Toutes les activités sportives'
+                                    ]
+                                ],
+                            ]
+                        ]
+                    ])
+                )
+            )
+        ),
+        new Delete()
+    ]
+)]
 class Tag
 {
     #[ORM\Id]
